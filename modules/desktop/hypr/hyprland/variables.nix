3ec2b6c5d9da979d57793a1c1964b04dfba0be6c
repinek <137826826+repinek@users.noreply.github.com>
2026-07-06 14:51:28 +0500

    style: format

--- modules/desktop/hypr/hyprland/variables.nix
+++ modules/desktop/hypr/hyprland/variables.nix
@@ -1,4 +1,4 @@
-{ hostname, ...}: {
+{hostname, ...}: {
   wayland.windowManager.hyprland.settings = {
     "$fileManager" = "nemo";
     "$terminal" = "alacritty";
@@ -7,11 +7,11 @@
     # Screenshots
     "$hyprpicker" = "hyprpicker --autocopy --no-fancy --render-inactive --scale=8";
 
-    "$screenshotDir"   = "/home/${hostname}/Media/Screenshots/$(date +%Y-%m)";
-    "$screenshotName"  = "Screenshot_$(date +%Y-%m-%d_%H-%M-%S).png";
+    "$screenshotDir" = "/home/${hostname}/Media/Screenshots/$(date +%Y-%m)";
+    "$screenshotName" = "Screenshot_$(date +%Y-%m-%d_%H-%M-%S).png";
     "$mkdirScreenshot" = "mkdir -p $screenshotDir";
-    "$hyprshotRegion"  = "$mkdirScreenshot && hyprshot --freeze --mode region --output-folder $screenshotDir --filename $screenshotName";
-    "$hyprshotSatty"   = "$mkdirScreenshot && hyprshot --freeze --mode region --raw | satty --filename - --output-filename $screenshotDir/$screenshotName";
-    "$hyprshotFull"    = "$mkdirScreenshot && hyprshot --freeze --mode output --mode DP-3 --output-folder $screenshotDir --filename $screenshotName"; # FIXME: remove hardcode
+    "$hyprshotRegion" = "$mkdirScreenshot && hyprshot --freeze --mode region --output-folder $screenshotDir --filename $screenshotName";
+    "$hyprshotSatty" = "$mkdirScreenshot && hyprshot --freeze --mode region --raw | satty --filename - --output-filename $screenshotDir/$screenshotName";
+    "$hyprshotFull" = "$mkdirScreenshot && hyprshot --freeze --mode output --mode DP-3 --output-folder $screenshotDir --filename $screenshotName"; # FIXME: remove hardcode
   };
 }
