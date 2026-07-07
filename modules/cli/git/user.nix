758c5b332d316edd8a658b27ab4d0782599f4313
repinek <137826826+repinek@users.noreply.github.com>
2026-07-07 22:47:07 +0500

    feat(git): configure git

--- modules/cli/git/user.nix
+++ modules/cli/git/user.nix
@@ -16,9 +16,36 @@ in {
         signByDefault = true;
       };
       settings = {
-        user.name = "repinek";
-        user.email = "137826826+repinek@users.noreply.github.com";
+        user = {
+          name = "repinek";
+          email = "137826826+repinek@users.noreply.github.com";
+        };
+
         gpg.format = "ssh";
+  
+        init = {
+          defaultBranch = "main";
+        };
+
+        push = {
+          autoSetupRemote = true; # Setup branch at remote if don't exist
+        };
+
+        pull = {
+          ff = "only"; # Use fast-forward - no merge commits
+        };
+        
+        rebase = {
+          autoStash = true;
+          autoSquash = true;
+        };
+
+        rerere.enabled = true;
+
+        help.autocorrect = 0; # You called y, which does not exist, we assume you meant x
+
+        log.date = "relative";
+        core.editor = "vim";
       };
     };
   };
