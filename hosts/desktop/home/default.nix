7f10c0c06170178347c5194424fb29aa2ee470a6
repinek <137826826+repinek@users.noreply.github.com>
2026-07-09 21:08:02 +0500

    Revert "feat: add Github Desktop"
    This reverts commit eda0a299c0c042114a2c0ebb7adbedf49a606471.

--- hosts/desktop/home/default.nix
+++ hosts/desktop/home/default.nix
@@ -16,7 +16,6 @@
     ../../../modules/games/osu-lazer/user.nix
 
     ../../../modules/programs/alacritty/user.nix
-    ../../../modules/programs/github-desktop/user.nix
     ../../../modules/programs/browsers/ungoogled-chromium/user.nix
     ../../../modules/programs/browsers/waterfox/user.nix
     ../../../modules/programs/losslesscut/user.nix
@@ -59,7 +58,6 @@
 
   modules.programs = {
     alacritty.user.enable = true;
-    github-desktop.user.enable = true;
     browsers = {
       ungoogled-chromium.user.enable = true;
       waterfox.user.enable = true;
