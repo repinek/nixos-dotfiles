a04b7bbed7b480349680b0f858d2e51f6a763693
repinek <137826826+repinek@users.noreply.github.com>
2026-08-05 18:59:35 +0500

    feat(hyprland): OBS Studio rules

--- modules/desktop/hypr/hyprland/rules.nix
+++ modules/desktop/hypr/hyprland/rules.nix
@@ -1,11 +1,17 @@
 _: {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
-      "match:class osu!, immediate yes" # For tearing
+      "match:class osu!, immediate true" # Allow tearing
 
       "match:class waterfox, workspace 1 silent"
 
       "match:class org.telegram.desktop, workspace 2 silent"
+
+      # Run OBS Studio silently on magic workspace, set float, size 1200 700 and center it
+      "match:class com.obsproject.Studio, workspace special:magic silent"
+      "match:class com.obsproject.Studio, float true"
+      "match:class com.obsproject.Studio, size 1200 700"
+      "match:class com.obsproject.Studio, center true"
     ];
   };
 }
