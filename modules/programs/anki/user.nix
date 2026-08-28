e3a6b711f3649f023462f44487d7d123c9ae3324
repinek <137826826+repinek@users.noreply.github.com>
2026-08-28 21:58:29 +0500

    feat(anki): configure

--- modules/programs/anki/user.nix
+++ modules/programs/anki/user.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: with lib;
 let 
@@ -11,6 +12,24 @@ in {
   config = mkIf cfg.enable {
     programs.anki = {
       enable = true;
+
+      language = "en_GB";
+
+      reduceMotion = true;
+      uiScale = 1.4;
+
+      addons = [
+        pkgs.ankiAddons.review-heatmap
+      ];
+
+      # FIXME: secrets
+      profiles."repinek" = {
+        default = true;
+
+        sync = {
+          autoSync = true;
+        };
+      };
     };
   };
 }
