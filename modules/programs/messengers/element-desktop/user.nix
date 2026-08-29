8ec99be40807e7f8acaea12967648c11bb237830
repinek <137826826+repinek@users.noreply.github.com>
2026-08-29 22:05:38 +0500

    style(element-desktop): remove fixme comment
    Nah, I'm too lazy for wrap app just for that, I don't think I need this

--- modules/programs/messengers/element-desktop/user.nix
+++ modules/programs/messengers/element-desktop/user.nix
@@ -9,7 +9,6 @@ in {
   options.modules.programs.messengers.element-desktop.user.enable = mkEnableOption "Element";
 
   config = mkIf cfg.enable {
-    # FIXME: Find a declarative way to set the password store
     # On first run, use --password-store=gnome-libsecret
     programs.element-desktop.enable = true;
   };
